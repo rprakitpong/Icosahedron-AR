ea5a8d9cfd4e6b62268a4e5905f8dd2ced07c539
rprakitpong <rprakitpong@gmail.com>
2018-11-28 14:02:19 -0800

    Initial commit

---
+++ Assets/Scripts/ShowHideToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideToggle : MonoBehaviour {
+
+    bool showing = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void whenClicked()
+    {
+        if (showing)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+            showing = false;
+        } else if (!showing)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+            showing = true;
+        }
+    }
+}
